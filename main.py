4f549eafef1414456e7872d18f061761aac303b2
Maria Akhmatgaleeva <mdakhmatgaleeva@edu.hse.ru>
2024-10-06 18:08:24 +0300

    Count processing time

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import cv2
 import dlib
+import time
 
-# Укажите путь к вашему видеофайлу
-video_path = 'input_files/input_video_1.mp4'
+# путь к input video
+video_path = 'input_files/input_video_5.mp4'
 video_capture = cv2.VideoCapture(video_path)
 
-# Инициализация детектора лиц
+# инициализация детектора лиц
 detector = dlib.get_frontal_face_detector()
 
-# Получаем параметры для записи выходного видео
+# получаем параметры для записи output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = 24
 
@@ -18,37 +19,45 @@ width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
+start_time = time.time()  # время начала
+
 while True:
-    # Захват кадра из видео
+    # читаем кадры из видео
     ret, frame = video_capture.read()
 
-    # Если кадры закончились, выйдите из цикла
     if not ret:
         break
 
-    # Преобразование в градации серого
+    # преобразование в монохром
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Обнаружение лиц
+    # обнаружение лиц
     rects = detector(gray, 0)
 
-    # Размываем лица
+    # размываем лица
     for rect in rects:
         x = rect.left()
         y = rect.top()
         x1 = rect.right()
         y1 = rect.bottom()
 
-        # Размытие области лица
+        # размытие области лица
         face_region = frame[y:y1, x:x1]
+
         blurred_face = cv2.GaussianBlur(face_region, (99, 99), 30)
         frame[y:y1, x:x1] = blurred_face
 
-    # Запись обработанного кадра в выходное видео
+    # записываем обработанный кадр в output video
     out.write(frame)
 
 
-# Освобождение ресурсов
+# овобождаем ресурсы
 video_capture.release()
 out.release()
 cv2.destroyAllWindows()
+
+end_time = time.time()  # время окончания
+processing_time = end_time - start_time  # Рассчитать время обработки
+
+cv2.destroyAllWindows()
+print(f"Время обработки видео: {processing_time:.2f} секунд")
